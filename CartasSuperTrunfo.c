577fae9b775749d1f143b2989275784226098665
passwordprograma <lininhaliveptc@gmail.com>
2025-02-26 17:56:35 +0000

    jogo de cartas salvar 123

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,7 @@ int main(){
     int pontosturisticos;
 
     printf("Digite o nome do estado: \n");
-    scanf("%s", &estado);
+    scanf("%s", &estado,);
     printf("Digite o codigo: \n");
     scanf("%s",&codigo);
     printf("Digite a população: \n");
@@ -25,31 +25,28 @@ int main(){
 }
 
 int main(){
-    // Carta2
-
-    char estado2;
-    char codigoCarta2;
-    int população;
-    float area;
-    float pib;
-    int pontosturisticos;
+    char estado1, estado2;
+    char codigo1,codigo2;
+    int população1,população2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosturisticos1, pontoturisticos2;
 
     printf("Digite o nome do estado: \n");
-    scanf("%s", &estado2);
+    scanf("%s", &estado1,&estado2);
     printf("Digite o codigo: \n");
-    scanf("%s",&codigoCarta2);
+    scanf("%s",&codigo1,&codigo2);
     printf("Digite a população: \n");
-    scanf("%d", &população);
+    scanf("%d", &população1,&população2);
     printf("Digite a Area: \n");
-    scanf("%f", &area);
+    scanf("%f", &area1,&area2);
     printf("Digite o Pib: \n");
-    scanf("%f",&pib);
+    scanf("%f",&pib1,&pip2);
     printf("Digite os Pontos Turisticos: \n");
-    scanf("%d", &pontosturisticos);
+    scanf("%d", &pontosturisticos1,&pontoturisticos2);
 
- return 0;
+    return 0;
 }
-
 
 
 
